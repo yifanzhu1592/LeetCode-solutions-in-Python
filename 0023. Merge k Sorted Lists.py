8f033db9c57693e30fc79830c1fddffeba1adcc6
Yifan Zhu <zhuyi@tcd.ie>
2022-01-23 23:56:06 +0000

    Create 0023. Merge k Sorted Lists.py

---
+++ 0023. Merge k Sorted Lists.py
@@ -0,0 +1,32 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+from queue import PriorityQueue
+
+class Solution():
+    def mergeKLists(self, lists):
+        """
+        :type lists: List[ListNode]
+        :rtype: ListNode
+        """
+        class Wrapper():
+            def __init__(self, node):
+                self.node = node
+            def __lt__(self, other):
+                return self.node.val < other.node.val
+        
+        head = point = ListNode(0)
+        q = PriorityQueue()
+        for l in lists:
+            if l:
+                q.put(Wrapper(l))
+        while not q.empty():
+            node = q.get().node
+            point.next = node
+            point = point.next
+            node = node.next
+            if node:
+                q.put(Wrapper(node))
+        return head.next
